9e8d22a3e125291d92868442b1785d8284ba42bb
lgmezabal <66521111+lgmezabal@users.noreply.github.com>
2023-04-13 11:55:12 -0300

    Update client.c

--- tp0/client/src/client.c
+++ tp0/client/src/client.c
@@ -48,7 +48,7 @@ int main(void)
 	conexion = crear_conexion(ip, puerto);
 
 	// Enviamos al servidor el valor de CLAVE como mensaje
-	enviar_mensaje("hola",conexion);
+	enviar_mensaje(valor,conexion);
 	// Armamos y enviamos el paquete
 	paquete(conexion);
 
